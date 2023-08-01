568ed9b3c73838593a9386fa30093046fc00f176
davupls <davidmiltonmclean@gmail.com>
2023-08-01 16:18:33 -0500

    Update main.js
    - mobile menu now appear and hides according to menu button

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ let menuButtonClosed = true;
 
 // Mobile menu variables
 const mobileMenu = document.querySelector(".menu-ul-mobile");
-let menuHeight = mobileMenu.offsetHeight;
+const menuHeight = (mobileMenu.offsetHeight + 2).toString();
+const negativeMenuHeight = "-" + menuHeight + "px";
+mobileMenu.style.top = negativeMenuHeight;
 
 // Switch mobile menu image
 menuButton.addEventListener("click", function(){
     if (menuButtonClosed === true) {
         menuImage.src = "assets/shared/mobile/icon-close.svg";
+        mobileMenu.style.top =  "0px";
         menuButtonClosed = false;
     } else {
         menuImage.src = "assets/shared/mobile/icon-hamburger.svg";
+        mobileMenu.style.top = negativeMenuHeight;
         menuButtonClosed = true;
     }
 });

f566bfb60eb932d3ee7460e094cb34b0e95586fe
davupls <davidmiltonmclean@gmail.com>
2023-08-01 16:00:49 -0500

    Mobile Menu manual position removed
    - remove top position value in css
    - main.js is able to get menu height value

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const menuImage = document.querySelector(".menu-btn-mobile-image");
 const menuButton = document.querySelector(".menu-btn-mobile");
 let menuButtonClosed = true;
 
+// Mobile menu variables
+const mobileMenu = document.querySelector(".menu-ul-mobile");
+let menuHeight = mobileMenu.offsetHeight;
+
 // Switch mobile menu image
 menuButton.addEventListener("click", function(){
     if (menuButtonClosed === true) {
@@ -12,8 +16,3 @@ menuButton.addEventListener("click", function(){
         menuButtonClosed = true;
     }
 });
-
-
-const mobileMenu = document.querySelector(".menu-ul-mobile");
-// let menuHeight = mobileMenu.he
-// offsetHeight
